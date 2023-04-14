0b0628b2c530b1bda7f6346ded7b51ef590a9e60
jaehee <121213522+jay0425@users.noreply.github.com>
2023-04-14 15:02:57 +0900

    0414 싸이월드 2탄 과제

---
+++ class/03-javascript/02-object-array.js
@@ -0,0 +1,15 @@
+const fruits = [
+  { number: 1, tilte: "레드향" },
+  { number: 2, tilte: "샤인머스켓" },
+  { number: 3, tilte: "산청딸기" },
+  { number: 4, tilte: "한라봉" },
+  { number: 5, tilte: "사과" },
+  { number: 6, tilte: "애플망고" },
+  { number: 7, tilte: "딸기" },
+  { number: 8, tilte: "천혜향" },
+  { number: 9, tilte: "과일선물세트" },
+  { number: 10, tilte: "귤" },
+];
+// undefined
+fruits[0].number + " " + fruits[0].tilte;
+// '1 레드향'
