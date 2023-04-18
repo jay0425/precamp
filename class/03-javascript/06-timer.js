fb487d7723df561ef2fce724ab22f0f4b3880be8
jaehee <121213522+jay0425@users.noreply.github.com>
2023-04-18 09:08:32 +0900

    0418

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,19 +1,32 @@
+let isStarted = false;
+
 const number = () => {
-  let result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
-  document.getElementById("target").innerText = result;
-  document.getElementById("target").style.color = "#" + result;
+  if (isStarted === false) {
+    // 타이머가 작동중이 아닐때
+    isStarted = true;
+    document.getElementById("finish").disabled = false;
 
-  let time = 180;
+    let result = String(Math.floor(Math.random() * 1000000)).padStart(6, "0");
+    document.getElementById("target").innerText = result;
+    document.getElementById("target").style.color = "#" + result;
 
-  setInterval(function () {
-    if (time >= 0) {
-      const min = Math.floor(time / 60);
-      const sec = String(time % 60).padStart(2, "0");
-      let result = min + ":" + sec;
-      document.getElementById("time").innerText = result;
-      time = time - 1;
-    } else {
-      document.getElementById("finish").disabled = true;
-    }
-  }, 1000);
+    let time = 10;
+    let timer;
+    timer = setInterval(function () {
+      if (time >= 0) {
+        const min = Math.floor(time / 60);
+        const sec = String(time % 60).padStart(2, "0");
+        let result = min + ":" + sec;
+        document.getElementById("time").innerText = result;
+        time = time - 1;
+      } else {
+        document.getElementById("finish").disabled = true;
+        isStarted = false;
+        console.log("타이머 작동중");
+        clearInterval(timer);
+      }
+    }, 1000);
+  } else {
+    // 타이머가 작동중일때
+  }
 };
